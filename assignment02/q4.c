6f05a4c179e8435df79ebbed13a33679ec949f48
khubaiburrehman0748 <khubaiburrehman993@gmail.com>
2024-10-30 03:19:41 -0700

    Update q4.c

--- assignment02/q4.c
+++ assignment02/q4.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-
-#define MAX_WORDS 99
-#define MAX_LENGTH 99
-void sortString(char* str) {
-    int len = strlen(str);
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (str[i] > str[j]) {
-                char temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
-            }
-        }
-    }
-}
-
 int main() {
-    int wordCount;
-    char transactions[MAX_WORDS][MAX_LENGTH];
-    char sortedTransactions[MAX_WORDS][MAX_LENGTH];
-    int grouped[MAX_WORDS] = {0}; 
-    printf("Enter the number of transactions: ");
-    scanf("%d", &wordCount);
-
-    for (int i = 0; i < wordCount; i++) {
-        printf("Transaction %d: ", i + 1);
-        scanf("%s", transactions[i]);
-
-        strcpy(sortedTransactions[i], transactions[i]);
-        sortString(sortedTransactions[i]);
+    char arr[99][99],newarr[99][99];
+    int total; 
+    printf("number of scrambled words? =");
+    scanf("%d",&total);
+    for(int i=0;i<total;i++){
+        printf("word %d =",i+1);
+        scanf("%s",&arr[i]);
     }
-
-    for (int i = 0; i < wordCount; i++) {
-        if (grouped[i]) continue; 
-
+    for(int i=0;i<total;i++){
+    strcpy( newarr[i], arr[i]);
+    }
+    int till=0,flag=0;
+    printf("[");
+    for(int i=0;i<total;i++){
+    if(flag==0){
+        till=strlen(arr[i]);
+        if(arr[i][0]!='\0'){
         printf("[");
-        printf("%s", transactions[i]);
-        grouped[i] = 1;
-
-        for (int j = i + 1; j < wordCount; j++) {
-            if (strcmp(sortedTransactions[i], sortedTransactions[j]) == 0) {
-                printf(", %s", transactions[j]);
-                grouped[j] = 1;
-            }
+        printf("%s",arr[i]);
         }
-        printf("]\n");
+        for(int l=0;l<till;l++){
+            for(int j=0;j<till-1;j++){
+                for(int k=j+1;k<till;k++){
+                    char temp=newarr[i][j];
+                    newarr[i][j]=newarr[i][k];
+                    newarr[i][k]=temp;
+                    for(int m=i+1;m<total;m++){
+                        int check=strcmp(newarr[i],arr[m]);
+                        if(check==0){
+                            printf(", %s",newarr[m]);
+                            arr[m][0]='\0';
+                        }
+                        else{
+                            flag=0;
+                        }
+                        }
+                    }
+                }    
+            }        
+        }
+        if(arr[i][0]!='\0'){
+        printf("]");
+        }
     }
+    printf("]");
 
     return 0;
 }
